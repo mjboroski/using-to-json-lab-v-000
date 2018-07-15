f40deac0ce340bb5bb9aab1d2c6fd0b19f35a1d3
Michael Boroski <mjboroski@gmail.com>
2018-07-15 22:14:31 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,6 @@ class ProductsController < ApplicationController
       format.html { render :show }
       format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
     end
-
   end
 
   def data
